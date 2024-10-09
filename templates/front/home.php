61b25ad05f12d76a0cae6e22b553c153bc8b6f4c
Edjek <edjek@hotmail.fr>
2024-10-09 11:55:38 +0200

    clean code

--- templates/front/home.php
+++ templates/front/home.php
@@ -1,7 +1,7 @@
 <section>
     <h1 class="display-3">Accueil</h1>
     <div class=" w-75 mx-auto my-5 p-3 border rounded">
-        <table class="table table-striped">
+        <table class="table table-striped table-hover">
             <tbody>
                 <?php foreach ($topics as $topic) { ?>
                     <tr>
